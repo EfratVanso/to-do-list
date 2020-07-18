2c9244f2a7e84dcfc8cc3941b63ac35de9e1bedc
efiVanso <efi.vanso@gmail.com>
2020-07-18 23:40:56 +0300

    Deleting Todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,12 @@
+//check off specific to do by clicking
 $("li").click(function () {
   $(this).toggleClass("completed");
+});
+
+//click on X to delete to do li 
+$("span").click(function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();// to not call to fathers events
 });
