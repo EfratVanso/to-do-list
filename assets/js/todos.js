f11bf4fbea92a2f00e9836ceb35db3e7c4761080
efiVanso <efi.vanso@gmail.com>
2020-07-18 23:51:04 +0300

    Creating Todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -10,3 +10,13 @@ $("span").click(function(event){
     });
     event.stopPropagation();// to not call to fathers events
 });
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        // grabbing new to do text from input
+       var todoText = $(this).val();
+       $(this).val("");
+       //create a new li and add to ul
+       $("ul").append("<li><span>X</span> "+ todoText +"</li>");
+    }
+});
